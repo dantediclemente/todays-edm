17a212b97770a8dc2061139983f0f9b536321c48
Dante DiClemente <dantediclemente@gmail.com>
2018-08-13 09:40:45 -0400

    update

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,13 +6,13 @@ export const Nav = () => {
     <div className='navbar-fixed'>
       <nav>
         <div className='nav-wrapper blue-grey darken-4'>
-          <Link to='/' className='brand-logo center'>
+          <Link to='/todays-edm' className='brand-logo center'>
             <span>TODAYS&rsquo; EDM</span>
           </Link>
           <ul id='nav-mobile' className='right hide-on-med-and-down'>
-            <li><Link to='/tracks'>Tracks</Link></li>
-            <li><Link to='/playlist'>Playlists</Link></li>
-            <li><Link to='/About'>About</Link></li>
+            <li><Link to='/todays-edm/tracks'>Tracks</Link></li>
+            <li><Link to='/todays-edm/playlist'>Playlists</Link></li>
+            <li><Link to='/todays-edm/About'>About</Link></li>
           </ul>
         </div>
       </nav>
