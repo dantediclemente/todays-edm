d285917c2b93440bb9a1506f899679ccb9cd1677
Dante DiClemente <dantediclemente@gmail.com>
2018-08-13 09:38:21 -0400

    update

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
       <Router>
         <div>
           <Nav />
-          <Route exact path='/' render={() => (
-            <Redirect to='/tracks' />
+          <Route exact path='/todays-edm' render={() => (
+            <Redirect to='/todays-edm/tracks' />
           )
           } />
-          <Route exact path='/tracks' component={TracksContainer} />
-          <Route exact path='/playlist' component={PlaylistsContainer} />
-          <Route exact path='/About' component={About} />
+          <Route exact path='/todays-edm/tracks' component={TracksContainer} />
+          <Route exact path='/todays-edm/playlist' component={PlaylistsContainer} />
+          <Route exact path='/todays-edm/About' component={About} />
         </div>
       </Router>
     )
